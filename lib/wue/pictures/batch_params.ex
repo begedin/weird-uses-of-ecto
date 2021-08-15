fa8a8ea5fd07b6f7d33c62d66a1be6e4569fa225
begedin <begedinnikola@gmail.com>
2021-08-15 19:00:13 +0200

    Tests for line shape

--- lib/wue/pictures/batch_params.ex
+++ lib/wue/pictures/batch_params.ex
@@ -1,6 +1,27 @@
 defmodule WUE.Pictures.BatchParams do
   @moduledoc """
   Params validator for any endpoint working with a list of pictures
+
+  This illustrates a API action pre-validation step as an atypical use for Ecto.
+
+  A lot of endpoints in a complex API will share a common params structure, at
+  least partially.
+
+  In this example, batch endpoints share the same format for filtering items in
+  that batch.
+
+  An embedded schema is used to define the structure of this format and cast it.
+  If the validation of the format fails, we return early from the controller
+  action, allowing us to skip any expensive operations and render a typical 422
+  validation errors response.
+
+  This is especially useful for endpoints with a shared params structure, but is
+  also useful for endpoints where several parameters are simply required to
+  perform an action and fetch some data.
+
+  There is finally also value in the fact that using this approach for any
+  endpoint requiring additional params which aren't encoded in the path will now
+  return a standardized response.
   """
 
   use Ecto.Schema
